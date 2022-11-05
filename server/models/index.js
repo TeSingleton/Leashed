186ba65cbad84169516bda7a94ab08cb5ce18815
colinplancich <colin.plancich@gmail.com>
2022-11-05 11:14:05 -0700

    Another fix attempt

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,4 @@
 const User = require('./User');
 const Pet = require('./Pet');
-const Location = require('./Location');
-const Message = require('./Message');
-const Trait = require('./Trait');
-const Breed = require('./Breed');
 
-
-
-
-module.exports = { User, Pet, Location, Message, Trait, Breed };
+module.exports = { User, Pet };
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,11 +1,7 @@
 const db = require('../config/connection');
 const { Tech } = require('../models');
 
-<<<<<<< HEAD
 const userData = require('./userData');
-=======
-const techData = require('./techData.json');
->>>>>>> 66992c9ebd911e62442956b7e01889e598871690
 
 db.once('open', async () => {
   await Tech.deleteMany({});
