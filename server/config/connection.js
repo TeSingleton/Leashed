477cb00fcaf6692dccf25ccc609d5443bc00a51e
colinplancich <colin.plancich@gmail.com>
2022-11-07 20:09:04 -0800

    updated DB name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/simple-timeline',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/leashed_DB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
