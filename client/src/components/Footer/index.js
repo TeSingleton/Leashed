88fd6a7c801d62a2dd3e1ac6f8611a1ff615fc84
Connor Mullan <cmullan602@gmail.com>
2022-11-12 11:21:38 -0700

    Leashed login changes and gallery lots of changes.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,14 +3,13 @@ import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import Paper from '@mui/material/Paper';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PetsIcon from '@mui/icons-material/Pets';
 import Auth from '../../utils/auth';
 
-export default function FixedBottomNavigation() {
+export default function Footer() {
   const [value, setValue] = React.useState(0);
   const ref = React.useRef(null);
 
@@ -28,8 +27,8 @@ export default function FixedBottomNavigation() {
             setValue(newValue);
           }}
         >
-          <BottomNavigationAction  icon={<AccountCircleIcon  fontSize="large"/>} href="/me"/>
-          <BottomNavigationAction  icon={<PetsIcon  fontSize="large"/>} href="/home"/>
+          <BottomNavigationAction  icon={<AccountCircleIcon  fontSize="large"/>} href="/profiles/:username"/>
+          <BottomNavigationAction  icon={<PetsIcon  fontSize="large"/>} href="/gallery"/>
           <BottomNavigationAction icon={<MailOutlineIcon fontSize="large"/>} href="/messages"/>
         </BottomNavigation>
         </>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_PET } from '../utils/queries';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import logo from '../Images/leashed-logo.png'
 
 
 
@@ -8,42 +10,23 @@ import { QUERY_PET } from '../utils/queries';
 // import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_PET, {
-    fetchPolicy: "no-cache"
-  });
-
-  const petList = data?.pets || [];
-
+  
   return (
-    <main>
-      <div className="card bg-white card-rounded w-50">
-        <div className="card-header bg-dark text-center">
-          <h1>Welcome to Leashed!</h1>
-        </div>
-        <div className="card-body m-5">
-          <h2>Our latest furry members:</h2>
-              {loading ? (
-                <div>Loading...</div>
-              ) : (
-                <ul className="square">
-                  {petList.map((pet) => {
-                    return (
-                      <li key={pet._id}>
-                      </li>
-                    );
-                  })}
-                </ul>
-              )}
-            </div>
-            <div className="card-footer text-center m-3">
-              <h2>Ready to create a new matchup?</h2>
-              <Link to="/matchup">
-                <button className="btn btn-lg btn-danger">Create Matchup!</button>
-              </Link>
-        </div>
-      </div>
-    </main>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Box>
+          <img id="img1" src={logo} alt="leashed logo"/>
+          <h1>Find local pets in your area and get leashed today!</h1>
+        </Box>
+        
+      </Container>
+    </React.Fragment>
   );
-};
+}
+
+
+
+
 
 export default Home;
