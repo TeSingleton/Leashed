8ba00f1d792e3633e4fa55d0e3a718003cc09617
Colin Plancich <97257142+cplancich@users.noreply.github.com>
2022-11-12 11:18:25 -0800

    Update Signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import UploadWidget from '../components/UploadWidget';
 
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+
+// TODO: create formState functions for Pet data (pet, breed, traits, pet bio, pet picture)
 
 const Signup = () => {
   const [formState, setFormState] = useState({
@@ -69,12 +72,50 @@ const Signup = () => {
                 />
                 <input
                   className="form-input"
-                  placeholder="******"
+                  placeholder="Password"
                   name="password"
                   type="password"
                   value={formState.password}
                   onChange={handleChange}
                 />
+                <input 
+                  className="form-input"
+                  placeholder="Your BFF's Name"
+                  name="pet"
+                  type="pet"
+                  value={formState.pet}
+                  onChange={handleChange}
+                />
+                <input 
+                  className="form-input"
+                  placeholder="Breed"
+                  name="breed"
+                  type="breed"
+                  value={formState.breed}
+                  onChange={handleChange}
+                />
+                <input 
+                  className="form-input"
+                  placeholder="Personality traits"
+                  name="traits"
+                  type="traits"
+                  value={formState.traits}
+                  onChange={handleChange}
+                />
+                <input 
+                  className="form-input"
+                  placeholder="Pet Bio"
+                  name="bio"
+                  type="bio"
+                  value={formState.bio}
+                  onChange={handleChange}
+                />
+                <div className="display-flex">
+                  {/* Used for uploading Pet Piccture */}
+                  {/* TODO: Deploy application to fix cloudinary bug (Does not run on local server, needs to be deployed) */}
+                  {/* TODO: Fetch data after upload to return cloudinary image link as a string to be attached to Pet */}
+                  <p>Pet Picture </p><UploadWidget />
+                </div>
                 <button
                   className="btn btn-block btn-primary"
                   style={{ cursor: 'pointer' }}
