bd121e2ef8c5a16d14e3ad760f8d0eddabdaa4f2
Ali Nugen <alexandra.nugen@gmail.com>
2022-11-13 21:35:21 -0800

    updating to include button on profile page to add pet

--- client/src/App.js
+++ client/src/App.js
@@ -85,7 +85,7 @@ function App() {
                 element={<Signup />}
               />
               <Route 
-                path="/petSignup"
+                path="/petsignup"
                 element={<PetSignup />}
               />
               <Route 
